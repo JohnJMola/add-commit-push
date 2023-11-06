5b3045d2fb3cab3b4869ec54a13af14fc945da8d
JohnJMola <scifiminer1000@gmail.com>
2023-11-05 23:04:14 -0600

    message

--- addcommitpush.py
+++ addcommitpush.py
@@ -1,11 +1,11 @@
 import os
 
-message=input("enter message")
+message=input("enter message hear")
 
 print('git status')
 os.system('git status')
 print('add file')
 os.system('git add -A')
-os.system('git commit -m "Update files"')
+os.system('git commit -m "message"')
 os.system('git push')
 print('upload successful')
